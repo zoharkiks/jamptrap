49d3a652aade3567bb3cce6ddff78f67af851eaa
Zohar Williams <zoharkiks@gmail.com>
2024-01-09 14:01:10 +0530

    Basic setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  server: { port: 4322 },
   integrations: [tailwind()]
 });
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        "accent-primary": "var(--color-neutral-silver)",
+        "surface-primary": "var(--color-neutral-black)",
+        "text-brand": "var(--color-brand-butterscotch)",
+        "text-primary": "var(--color-neutral-white)",
+      },
+    },
+	fontFamily: {
+		'oswald': ['Oswald', 'sans-serif'],
+	}
+  },
+  
+  plugins: [],
+};
