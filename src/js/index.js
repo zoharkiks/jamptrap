a40b156b680af12fd226d11c0fefb70ddae02307
Zohar Williams <zoharkiks@gmail.com>
2024-01-11 00:46:05 +0530

    Add videos

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,18 @@
-// Js
+import Lenis from "@studio-freight/lenis";
+
+initScripts();
+
+function initSmoothScroll(params) {
+  const lenis = new Lenis();
+
+  function raf(time) {
+    lenis.raf(time);
+    requestAnimationFrame(raf);
+  }
+
+  requestAnimationFrame(raf);
+}
+
+function initScripts() {
+  initSmoothScroll();
+}
